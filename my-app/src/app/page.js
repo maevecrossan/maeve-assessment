b4861ec6139c12a8f75409f7b51c691f38d5aa54
Maeve Crossan <maevecrossan@gmail.com>
2025-11-26 11:21:09 +0000

    Display the homepage to users who are logged in. Display personalised message using the user's full name. Add logout button below message. Format file.

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,21 +1,61 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-green-50 font-sans">
-      <main className="flex min-h-screen w-full items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h2 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
-            Welcome!
-          </h2>
-          <p className="max-w-sm text-lg leading-7 text-gray-700">  
-            Please log in or create an account to continue.
-          </p>
-          <div className="flex gap-4">
-            <a href="/login" className="rounded-md bg-green-600 px-4 py-2 text-sm font-medium text-white hover:bg-green-700">
-              Log In
-            </a>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+
+	const [user, setUser] = useState(null);
+	const router = useRouter();
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			if (!currentUser) {
+				router.push('/login');
+			} else {
+				setUser(currentUser);
+			}
+		});
+
+		return () => unsubscribe();
+	}, [router]);
+
+	// Logout Handler
+	const handleLogout = async () => {
+		try {
+			await signOut(auth);
+			router.push('/login');
+		}
+		catch (error) {
+			console.error("Error signing out: ", error);
+		}
+	};
+
+	// Prevent UI from rendering before Firebase responds
+	if (!user) return <p className="p-5">Loading...</p>;
+
+	return (
+		<div className="flex items-center justify-center bg-green-50 font-sans">
+			<main className="flex w-full items-center justify-center py-42 px-16 bg-white">
+				<div className="flex flex-col items-center gap-6 text-center">
+					<h2 className="max-w-lg text-3xl font-semibold leading-10 tracking-tight text-black">
+						Hey, {user.displayName}! You&apos;re successfully logged in 🎉
+					</h2>
+
+					<p className="max-w-lg text-lg leading-7 text-gray-700">
+						Welcome back.
+					</p>
+
+					<button
+						onClick={handleLogout}
+						className="rounded-md bg-red-600 px-4 py-2 text-sm font-medium text-white hover:bg-red-700"
+					>
+						Logout
+					</button>
+				</div>
+			</main>
+		</div>
+	);
 }
