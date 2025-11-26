d64e796b5b3c520cda035e9949efe15ffb95c113
Maeve Crossan <maevecrossan@gmail.com>
2025-11-26 11:22:04 +0000

    Update login button to switch to logout button depending on user auth status. Update button styles.

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,17 +1,44 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+
 export default function Navbar() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+    }, []);
+
+    const handleLogout = async () => {
+        await signOut(auth);
+    };
+
     return (
         <nav className="w-full p-4 bg-gray-800 text-white">
             <a href="/">
                 <h1 className="text-2xl font-bold m-2">My App</h1>
             </a>
 
-            {/* Placeholder */}
-            <div className="absolute top-4 right-4 mt-2.5">
-                <a href="/login" className="px-3 py-2 rounded bg-green-200 text-green-700">
-                    Login
-                </a>
+            {/* Conditional rendering based on user authentication */}
+            <div className="absolute top-4 right-4">
+                {user ? (
+                    <button
+                        onClick={handleLogout}
+                        className="px-3 py-2 rounded bg-red-200 text-red-700"
+                    >
+                        Logout
+                    </button>
+                ) : (
+                    <Link href="/login" className="block px-3 py-2 rounded bg-green-200 text-green-700">
+                        Login
+                    </Link>
+                )}
             </div>
         </nav>
     );
