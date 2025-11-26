4c6da642693839d9a1eda6e0b9d4b32012bf89d9
Maeve Crossan <maevecrossan@gmail.com>
2025-11-26 10:30:12 +0000

    Downgrade heading level.

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-green-50 font-sans">
       <main className="flex min-h-screen w-full items-center justify-between py-32 px-16 bg-white sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
+          <h2 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
             Welcome!
-          </h1>
+          </h2>
         </div>
       </main>
     </div>
